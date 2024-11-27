042673823ba839f9e7c4de4a4f36af4658610d7a
Suraj Namdev gitte <158551269+suraj126708@users.noreply.github.com>
2024-11-27 21:13:19 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,17 +66,6 @@ function Navbar() {
               </li>
               <li className="nav-item"></li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-primary" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
